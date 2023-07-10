1bb6c02e2c7a8281bbee0679c15a135e0c5e95df
heyq <yq9932@qq.com>
2023-07-10 09:14:08 +0800

    feat: 预设背景图片，但无法实时显示

--- src/views/utils/render.tsx
+++ src/views/utils/render.tsx
@@ -13,6 +13,8 @@ import getOptions from "@/controller/apis/getOptions";
 import Matrix from "@/controller/libs/types/Matrix.ts";
 import Options from "@/controller/libs/types/Options.ts";
 
+import bgi from "/bgi.jpg";
+
 function listPoint(matrix: Matrix): Array<JSX.Element> {
 	if (!matrix) return [];
 
@@ -37,6 +39,7 @@ function calViewBox(matrix: Matrix) {
 	if (!matrix) return "0 0 0 0";
 
 	const nCount = matrix.getModuleCount();
+	return nCount;
 	return "0 0 " + String(nCount) + " " + String(nCount);
 }
 
@@ -45,6 +48,8 @@ const Index = () => {
 	const [matrix, setMatrix] = useState<Matrix>(getMatrix());
 	const [options, setOptions] = useState<Options>(getOptions());
 
+	const count = matrix.getModuleCount();
+
 	store.subscribe(() => {
 		setMatrix(getMatrix());
 		console.log("二维码更新成功");
@@ -52,17 +57,28 @@ const Index = () => {
 	});
 
 	return (
-		<svg
-			className="Qr-item-svg"
-			width={options.size}
-			height={options.size}
-			viewBox={calViewBox(matrix)}
-			fill="white"
-			xmlns="http://www.w3.org/2000/svg"
-			xmlnsXlink="http://www.w3.org/1999/xlink">
-				<rect fill={options.foreColor} width={1} height={1} id="fore"/>
-				<rect fill={options.backColor} width={1} height={1} id="back"/>
-				{listPoint(matrix)}
+		// <svg
+		// 	style={style}
+		// 	className={"base"}
+		// 	width={options.size}
+		// 	height={options.size}
+		// 	viewBox={calViewBox(matrix)}
+		// 	xmlns="http://www.w3.org/2000/svg">
+		// 		<rect fill={"blue"} width={options.size} height={options.size} id={"bgi"}></rect>
+		// 		<rect fill={options.foreColor} width={1} height={1} id="fore"/>
+		// 		<rect fill={options.backColor} width={1} height={1} id="back"/>
+		// 		{listPoint(matrix)}
+		// 		<use href="#bgi" x="0" y={"0"} width={options.size} height={options.size} fill={"red"} />
+		// 		<circle id="myCircle" cx="5" cy="5" r="4" stroke="blue" />
+		// 		<use href="#myCircle" x="10" fill="blue" />
+		// 	<image href="https://pic.imgdb.cn/item/64ab53ec1ddac507cca53887.jpg" height="200" width="200" />
+		// </svg>
+		<svg width={options.size} height={options.size} xmlns="http://www.w3.org/2000/svg" viewBox={`0 0 ${count} ${count}`}>
+			{/*<image href="http://localhost:5173/bgi.jpg" width={count} height={count} />*/}
+			<image href={"http://localhost:5173/bgi.jpg"} width={count} height={count} />
+			<rect fill={options.foreColor} width={1} height={1} id="fore"/>
+			{/*<rect fill={options.backColor} width={1} height={1} id="back"/>*/}
+			{listPoint(matrix)}
 		</svg>
 	);
 };
